0577ab13ab2a79170eeab47083a15fca30278105
Josselin BUILS <josselin.buils@gmail.com>
2017-10-08 20:08:47 +0200

    Revert db dev host

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ const Logger = require('./logger');
 module.exports = class DB {
     static init() {
         Logger.info('Initialize database');
-        mongoose.connect('mongodb://josselinbuils.me:27017/pizza-day');
+        mongoose.connect('mongodb://localhost/pizza-day');
         mongoose.connection.on('error', error => Logger.error('An error occurred with the DB connection: ' + error.stack));
     }
 };
